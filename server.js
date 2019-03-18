fc001f1e843271828aa48f3c8ff31826f0b5fcdd
Redhairedgadget <32018908+Redhairedgadget@users.noreply.github.com>
2019-03-18 16:19:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,66 @@
-const cors = require('cors');
-const express = require('express');
-const app = express();
 
-app.use(cors());
+// init project
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var mongoose = require('mongoose');
+var shortUrl = require('./models/short');
+
+//Connect to database
+
+mongoose.connect(process.env.MONGOLAB_URI, {useMongoClient: true})
+
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
-// get ip infos even if passing through a proxy like here
-app.enable('trust proxy'); 
+app.use(bodyParser.urlencoded({'extended': false}));
+app.use(cors());
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/api/whoami', function(req, res, next){
-  res.json({ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
+//Using POST to get data from input and manipulate it
+app.post('/new', function(req, res){
+ 
+  var short = Math.floor(Math.random()*100000).toString();
+  var data = new shortUrl({
+    original_url: req.body.url,
+    short_url: short
+  });
+  
+  data.save(err=> {
+    if(err){
+      return err;
+    }
+  });
+  
+  return res.json(data);
 });
 
-// 404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-})
+//Check db and forward to original url
+app.get('/new/:urlToSend', function(req, res, next){
+  var numberUrl = req.params.urlToSend;
+  
+  shortUrl.findOne({'short_url' : numberUrl}, function(err, data){
+    if(err){
+      return err
+    }else{
+      res.redirect(301, data.original_url);
+    }
+  })
+});
+
+
+
+// Answer not found to all the wrong routes
+app.use(function(req, res, next){
+  res.status(404);
+  res.type('txt').send('Not found');
+});
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
